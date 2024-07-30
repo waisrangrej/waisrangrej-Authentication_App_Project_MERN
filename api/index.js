295d22785aa0673ccf9e06e72b03da67cbd835ff
waisrangrej <awaisrangrej9876@gmail.com>
2024-07-31 03:21:24 +0530

    created the server

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import { log } from 'console'
+import express from 'express'
+
+const app =express()
+const port=process.env.PORT || 3000
+app.listen(port,(req,res)=>{
+    console.log(`server listening on port ${port}`);
+})
